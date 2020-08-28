7f7f156ce78dfc73785efd037d051b1654d1e317
dalong <dalong0514@126.com>
2020-08-28 17:57:42 +0800

    数据库模型匹配的入口文件修改

--- www/lara/public/index.php
+++ www/lara/public/index.php
@@ -1,12 +1,20 @@
 <?php
 
 // 调用自动加载文件，添加自动加载文件函数
+
+use Illuminate\Database\Capsule\Manager;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 // 实例化服务组件，注册事件、路由服务提供者
 $app = new Illuminate\Container\Container;
 with(new Illuminate\Events\EventServiceProvider($app))->register();
 with(new Illuminate\Routing\RoutingServiceProvider($app))->register();
+
+// 启动 Eloquent ORM 模块并进行相关配置
+$manager = new Manager();
+$manager->addConnection(require '../config/database.php');
+$manager->bootEloquent();
 
 // 加载路由
 require __DIR__ . '/../app/Http/routes.php';
