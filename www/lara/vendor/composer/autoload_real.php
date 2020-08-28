c7c2809231ab411fbb643c491d262776e9ac398b
dalong <dalong0514@126.com>
2020-08-28 16:15:20 +0800

    添加路由组件

--- www/lara/vendor/composer/autoload_real.php
+++ www/lara/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitd5368c0ba596ef005f89a2d29f7499a9
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitd5368c0ba596ef005f89a2d29f7499a9::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired5368c0ba596ef005f89a2d29f7499a9($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequired5368c0ba596ef005f89a2d29f7499a9($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
